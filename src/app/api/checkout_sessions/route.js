14e0e7e4834b7e7bad76acb9b18417766fbb39b5
ruhul2003 <ruhul941020@gmail.com>
2026-06-26 21:21:27 +0600

    Added one origin

--- src/app/api/checkout_sessions/route.js
+++ src/app/api/checkout_sessions/route.js
@@ -5,9 +5,7 @@ import { stripe } from '../../../lib/stripe';
 export async function POST(request) {
   try {
     const headersList = await headers();
-    const origin =
-  headersList.get("origin") ||
-  process.env.NEXT_PUBLIC_APP_URL;
+    const origin = process.env.BETTER_AUTH_URL;
 
     const formData = await request.formData();
     const bookId = formData.get('bookId');
